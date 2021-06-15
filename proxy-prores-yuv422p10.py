360c5e4bb7b17fcba7039b88c60881ee3512edeb
johan <stjernholm@msn.com>
2021-06-15 14:48:31 +0800

    ffmpg prores conversion

---
+++ proxy-prores-yuv422p10.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+def main():
+    # Get info from user
+    directory = input("Path for files: ")
+    
+    for filename in os.listdir(directory):
+        # extract file prefix and suffix
+        file_parts = filename.split('.')
+        prefix = file_parts[0]
+
+        subprocess.call(
+            f"/usr/bin/ffmpeg -i {directory}/{filename} -c:v prores_ks -profile:v 2 -vendor apl0 -bits_per_mb 8000 -pix_fmt yuv422p10le -c:a copy {directory}/{prefix}.mov".split()
+            )
+
+
+if __name__ == '__main__':
+    main()
