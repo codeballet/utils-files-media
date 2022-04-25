7b556ccef0d42e12b50b73d1a9869e230f55799c
johan <stjernholm@msn.com>
2022-04-25 21:45:24 +0800

    Feature: rename-media.py user info

--- rename-media.py
+++ rename-media.py
@@ -4,45 +4,64 @@ import re
 
 def cleanup(text):
     '''
-    Cleans up user input.
     Replaces whitespace and hyphen with underscore.
-    Exits if non-alphanunerical characters are used.
     Lowers all letters.
     '''
     text = text.replace(" ", "_")
     text = text.replace("-", "_")
-    if not re.match(r'^[a-zA-Z0-9_]+$', text):
-        print('\nPlease only use letters, numbers, and underscore "_".')
-        exit()
     return text.lower()
+
+
+def check_input(text):
+    '''
+    Returns True if input only contains alphanumerical characters.
+    Returns False otherwise.
+    '''
+    if re.match(r'^[a-zA-Z0-9_]+$', text):
+        # only alphanumerical characters
+        return True
+
+    # other characters used
+    print('\nPlease only use letters, numbers, and underscore "_".')
+    return False
 
 
 def get_info(choice):
     '''
     Collects information from user for new filename
     '''
-    # Get information from user
-    directory = input("\nFile location: ")
-    # remove end backslash, if existing
-    re.sub(r'\/$', '', directory)
+    while True:
+        # Get information from user
+        directory = input("\nFile location: ")
+        # remove end /, if existing
+        re.sub(r'\/$', '', directory)
 
-    if not os.path.exists(directory):
-        # given directory does not exist
-        print('The given directory does not exist!')
-        exit()
+        if os.path.exists(directory):
+            break
+        print('\nThat directory does not exist!')
 
     if choice != 3:
         # user wants to change filenames, get more info
-        date = input("Date (yymmdd): ")
-        if not re.match(r'^\d{6}$', date):
-            print("Please enter the date as yymmdd, using six digits.")
-            exit()
+        while True:
+            date = input("\nDate (yymmdd): ")
+            if re.match(r'^\d{6}$', date):
+                break
+            print("\nPlease enter the date as yymmdd, using six digits.")
 
-        project = cleanup(input("Project name: "))
+        while True:
+            project = cleanup(input("\nProject name: "))
+            if check_input(project):
+                break
 
-        source = cleanup(input("Source (camera, person, organisation, etc.): "))
+        while True:
+            source = cleanup(input("\nSource (camera, person, organisation, etc.): "))
+            if check_input(source):
+                break
 
-        info = cleanup(input("Other info (card number, colour space, location, etc.): "))
+        while True:
+            info = cleanup(input("\nOther info (card number, colour space, location, etc.): "))
+            if check_input(info):
+                break
 
         # Give feedback to user
         print(f"\nYou will be changing filenames in the folder:\n{directory}")
@@ -159,13 +178,15 @@ def main():
     print('1. Keep original filenames when renaming')
     print('2. Discard original filenames when renaming')
     print('3. Undo filename change from 1, or only keep count from 2')
-    choice = input('\nChoice: ')
 
-    # check user input
-    if not re.match(r'^[123]$', choice):
+    while True:
+        choice = input('\nChoice: ')
+        # check user input
+        if re.match(r'^[123]$', choice):
+            break
         print('Please enter one digit: 1, 2, or 3')
-        exit()
 
+    # verify user input
     response = False
     choice = int(choice)
 
