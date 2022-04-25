7cd55df6f0a08a4875eafa047310b3f129dc55dd
johan <stjernholm@msn.com>
2022-04-25 21:03:58 +0800

    Fix: pdf-merge.py user info handling

--- pdf-merge.py
+++ pdf-merge.py
@@ -3,12 +3,19 @@ import re
 from PyPDF2 import PdfFileMerger
 
 def check_suffix(filename):
+    '''
+    Check output file for .pdf suffix.
+    Add suffix if not existing.
+    '''
     if not re.match(r'\.pdf$', filename):
         return '.'.join([filename, 'pdf'])
     return filename
 
 
 def merge(files, output):
+    '''
+    Merge and write the files
+    '''
     merger = PdfFileMerger()
 
     for pdf in files:
@@ -19,10 +26,22 @@ def merge(files, output):
 
 
 def path_check(path):
-    if not os.path.exists(path):
-        # directory does not exist
-        print(f'The directory {path} does not exist!')
-        exit()
+    '''
+    Check if file path exists
+    '''
+    if os.path.exists(path):
+        return True
+    
+    # directory does not exist
+    print(f'The directory {path} does not exist!')
+    return False
+
+
+def remove_slash(path):
+    '''
+    Remove slash at the end of path
+    '''
+    return re.sub(r'\/$', '', path)
 
 
 def main():
@@ -30,18 +49,23 @@ def main():
     Merge pdf files
     '''
     print('This program merges pdf files from a directory')
-    source_dir = input('\nLocation of pdf files:\n')
-    output_dir = input('\nWhere do you want to save the file?\n')
+    
+    while True:
+        source_dir = input('\nLocation of pdf files:\n')
+        source_dir = remove_slash(source_dir)
+        if path_check(source_dir):
+            # path valid
+            break
+
+    while True:
+        output_dir = input('\nWhere do you want to save the file?\n')
+        output_dir = remove_slash(output_dir)
+        if path_check(output_dir):
+            # path valid
+            break
+
     merged_file = input('\nWhat do you want to call the merged file?\n')
     
-    # remove end backslash, if existing on paths
-    re.sub(r'\/$', '', source_dir)
-    re.sub(r'\/$', '', output_dir)
-
-    # check if paths are valid
-    path_check(source_dir)
-    path_check(output_dir)
-
     # make sure merged_file has .pdf suffix
     merged_file = check_suffix(merged_file)
 
